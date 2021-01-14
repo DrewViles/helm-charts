63750c18317bee6c667dc70f1abd41c56edd3b59
Drew <vilesy@gmail.com>
2021-01-14 12:49:15 +0000

    updating executable permission for shell fiels

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -8,6 +8,7 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
+chmod +x ./kubeval
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
